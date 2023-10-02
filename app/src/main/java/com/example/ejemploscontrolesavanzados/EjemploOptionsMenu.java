6cdd5af30095ce105436d6b336b00612c70a5b90
Fernando <fvazquez.docencia@gmail.com>
2023-10-02 17:42:25 +0200

    Subida

--- app/src/main/java/com/example/ejemploscontrolesavanzados/EjemploOptionsMenu.java
+++ app/src/main/java/com/example/ejemploscontrolesavanzados/EjemploOptionsMenu.java
@@ -2,8 +2,14 @@ package com.example.ejemploscontrolesavanzados;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class EjemploOptionsMenu extends AppCompatActivity {
 
@@ -12,11 +18,29 @@ public class EjemploOptionsMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejemplo_options_menu);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.option_menu_1,menu);
+
+        menu.add("Opcion A desde código");
+        menu.add("Opcion B desde código");
+
+        SubMenu sMenu1 = menu.addSubMenu("Submenu C desde Java");
+        sMenu1.add("Opcion CA desde código");
+        sMenu1.add("Opcion CB desde código");
+
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(getBaseContext(), item.getTitle(), Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
     }
 }
