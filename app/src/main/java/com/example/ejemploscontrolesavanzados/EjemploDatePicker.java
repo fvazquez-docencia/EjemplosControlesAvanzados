2d162511e7ab2ec8c2790e6fe26de87b9ec8d437
fvazquez_docencia <fvazquez.docencia@gmail.com>
2023-10-02 22:08:18 +0200

    Mas ejemplos

--- app/src/main/java/com/example/ejemploscontrolesavanzados/EjemploDatePicker.java
+++ app/src/main/java/com/example/ejemploscontrolesavanzados/EjemploDatePicker.java
@@ -1,6 +1,13 @@
 package com.example.ejemploscontrolesavanzados;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +17,29 @@ public class EjemploDatePicker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejemplo_date_picker);
+
+
+        Button btEscogeFecha =( Button) findViewById(R.id.btEscogerFecha);
+
+        btEscogeFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int anho = cal.get(Calendar.YEAR);
+                int mes = cal.get(Calendar.MONTH);
+                int dia = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dpFecha= new DatePickerDialog(view.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int anhoSel, int mesSel, int diaSel) {
+                        TextView tvFecha = (TextView) findViewById(R.id.tvFechaEscogida);
+                        tvFecha.setText(diaSel + "/" + mesSel + "/" + anhoSel);
+
+                    }
+                },anho,mes,dia);
+
+                dpFecha.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ejemploscontrolesavanzados/EjemploTimePicker.java
+++ app/src/main/java/com/example/ejemploscontrolesavanzados/EjemploTimePicker.java
@@ -1,6 +1,13 @@
 package com.example.ejemploscontrolesavanzados;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +17,28 @@ public class EjemploTimePicker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejemplo_time_picker);
+
+        Button btEscogeHora =( Button) findViewById(R.id.btEscogerHora);
+
+        btEscogeHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hora = cal.get(Calendar.HOUR);
+                int minuto = cal.get(Calendar.MINUTE);
+                TimePickerDialog dpHora = new TimePickerDialog(view.getContext(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int horaSel, int minutoSel) {
+                                TextView tvFecha = (TextView) findViewById(R.id.tvHoraEscogida);
+                                tvFecha.setText(horaSel + ":" + minutoSel);
+                            }
+
+
+                        },hora,minuto,true);
+
+                dpHora.show();
+            }
+        });
     }
 }
